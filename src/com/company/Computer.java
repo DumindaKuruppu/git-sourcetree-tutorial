586b9006c35f6682ec65625213c905b90975d15e
dulithaph <duliherath70@gmail.com>
2022-02-11 17:35:33 +0530

    Update MickieComputer Class

--- src/com/company/Computer.java
+++ src/com/company/Computer.java
@@ -7,4 +7,5 @@ public interface Computer {
     void switchOnOff();
 
 
+
 }
--- src/com/company/MickieComputer.java
+++ src/com/company/MickieComputer.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.*;
 import java.util.Scanner;
+
+import java.time.LocalTime;
 
 public class MickieComputer implements Computer {
 
+    String name;
     String brand;
     String ram ;
     String processor;
@@ -17,11 +21,41 @@ public class MickieComputer implements Computer {
 
 
 
+    public  void computerDetails(){
+
+        System.out.print("Please Enter your name ");
+        name = input.next();
+
+        System.out.print("What is Brand of your Lap " + type + ": ");
+        brand = input.next();
+
+        System.out.print("What is Ram size:");
+        ram = input.next();
+
+        System.out.print("What is the Processor :");
+        processor = input.next();
+
+        System.out.print("What is the OS Currently use :");
+        os = input.next();
+
+        System.out.print("Generation :");
+        gen = input.nextInt();
+
+
+
+        System.out.println( name + "'s Lap is  " + brand + " And "+ ram + " Gb Ram, " + processor + " Processor with " + os +" OS ,, " + gen + " th gen");
+
+    }
+
+
 
     @Override
     public void switchOnOff(){
 
-        System.out.print("what is the time ??? ");
+        LocalTime time1 = LocalTime.now();
+        System.out.println(time1);
+
+        System.out.print("What is the Time ??? ");
         time = input.nextInt();
 
         if (time > 9 && time < 18 ){
@@ -34,34 +68,6 @@ public class MickieComputer implements Computer {
             System.out.println("You can not switch on lap");
         }
 
-    }
-
-
-
-    public  void computerDetails(){
-
-        System.out.print("Enter Brand of your " + type + ": ");
-        brand = input.next();
-
-        System.out.print("Ram :");
-        ram = input.next();
-
-        System.out.print("Processor :");
-        processor = input.next();
-
-        System.out.print("OS Type :");
-        os = input.next();
-
-        System.out.print("Generation :");
-        gen = input.nextInt();
-
-
-
-        System.out.println("Brand of Lap is  " + brand);
-        System.out.println("RAM of Lap is  " + ram);
-        System.out.println("Processor of Lap is  " + processor);
-        System.out.println("Os  is  " + os);
-        System.out.println("Generation  is  " + gen);
     }
 
 
