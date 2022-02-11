8cecfbb8f2e5196223fdeb6fe6af4e748a87ae49
Duminda Kuruppu <kuruppu.duminda@gmail.com>
2022-02-11 16:47:23 +0530

    created an arraylist of objects using a for loop to enter intern data

--- src/com/company/InternsRecruiter.java
+++ src/com/company/InternsRecruiter.java
@@ -1,21 +1,45 @@
 package com.company;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class InternsRecruiter {
+    Scanner scanner = new Scanner(System.in);
 
-    //        MickieIntern duminda = new MickieIntern();
+    public void internRecruiter() {
 
-//        duminda.setName("Duminda");
-//        duminda.setIdNumber("943663718V");
-//        duminda.setSex("Male");
-//        duminda.setAge(27);
-//
-//        duminda.work();
-//        duminda.drink("Whiskey");
-//        duminda.play("Cricket");
+        ArrayList<MickieIntern> interns = new ArrayList<>();
 
-    ArrayList<MickieIntern> interns = new ArrayList<>();
+        System.out.print("Please enter number of interns you want to recruit : ");
+        int numberOfInterns = scanner.nextInt();
 
-        
+        for (int i = 0; i < numberOfInterns; i++) {
+            interns.add(new MickieIntern());
+
+            System.out.print("Please enter " +(i+1)+ " intern name : ");
+            interns.get(i).setName(scanner.next());
+
+            System.out.print("Please enter intern ID number : ");
+            interns.get(i).setIdNumber(scanner.next());
+
+            System.out.print("Please enter intern Sex : ");
+            interns.get(i).setSex(scanner.next());
+
+            System.out.print("Please enter intern Age : ");
+            interns.get(i).setAge(scanner.nextInt());
+
+        }
+
+
+
+        for (int i = 0; i < interns.size(); i++) {
+            System.out.println(interns.get(i).getName());
+        }
+
+
+
+
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        InternsRecruiter mickie = new InternsRecruiter();
 
+        mickie.internRecruiter();
     }
 }
