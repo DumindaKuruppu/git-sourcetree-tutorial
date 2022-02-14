1feb18d5529d0ebd228d66f2f34ace6c95e07300
Eeshan709 <eeshanpoornima97@gmail.com>
2022-02-14 15:52:35 +0530

    Main method cleaned.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        InternsRecruiter mickie = new InternsRecruiter();
-
-//        this is my branch
-
-        mickie.internRecruiter();
+        
     }
 }
